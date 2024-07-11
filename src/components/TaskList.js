97d70d58161bcd6881dc2d60a2e2687802437eb4
MohammadFaisal012000 <99781839+MohammadFaisal012000@users.noreply.github.com>
2024-07-11 15:11:39 +0530

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,13 +5,14 @@ import { deleteTask, editTask } from "../redux/actions";
 import "./TaskList.css";
 
 const TaskList = () => {
+  // here we use selector for knowing about the state  and dispatch is use for reducer function
   const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-
+// delete task 
   const handleDelete = (taskId) => {
     dispatch(deleteTask(taskId));
   };
-
+// Edit task
   const handleEdit = (task) => {
     const newText = prompt("Edit your task", task.text);
     if (newText) {
@@ -25,9 +26,11 @@ const TaskList = () => {
         <div key={task.id} className="tasklist">
           {task.text}
           <div>
+  // onClick handler is used for edit
             <button onClick={() => handleEdit(task)} className="edit-button">
               Edit
             </button>
+    // onClick handler is used for delete
             <button
               onClick={() => handleDelete(task.id)}
               className="delete-button"
