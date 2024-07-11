905edd362831cf1b693b38106c64c092ba049da9
MohammadFaisal012000 <99781839+MohammadFaisal012000@users.noreply.github.com>
2024-07-11 15:04:31 +0530

    Update TaskInput.js

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -5,7 +5,9 @@ import { addTask } from "../redux/actions";
 import "./TakeInput.css";
 
 const TaskInput = () => {
+  // use State is use here for updating the value
   const [task, setTask] = useState("");
+  // dispatch is used here for using reduce function like add
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
@@ -18,6 +20,7 @@ const TaskInput = () => {
 
   return (
     <div className="head-takeinput">
+    // make form for ease to take the value and add it todo list
       <form onSubmit={handleSubmit}>
         <input
           type="text"
