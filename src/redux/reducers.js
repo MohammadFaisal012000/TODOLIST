c037f37a94fb059dcdbd74f2c97c8f2792860950
MohammadFaisal012000 <99781839+MohammadFaisal012000@users.noreply.github.com>
2024-07-11 15:01:07 +0530

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,22 +1,27 @@
-// src/redux/reducers.js
+// firstly import the task
 import { ADD_TASK, DELETE_TASK, EDIT_TASK } from "./actions";
 
+// initial state define
 const initialState = {
   tasks: [],
 };
 
+// Using Switch for doing all three task add ,delete and edit 
 const taskReducer = (state = initialState, action) => {
   switch (action.type) {
+      //  adding 
     case ADD_TASK:
       return {
         ...state,
         tasks: [...state.tasks, action.payload],
       };
+      // Deleting use filter method
     case DELETE_TASK:
       return {
         ...state,
         tasks: state.tasks.filter((task) => task.id !== action.payload),
       };
+      // edit  
     case EDIT_TASK:
       return {
         ...state,
